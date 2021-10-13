92c4e57c74f72d7210b420effb0da1c4637685b5
AxonBlAzE <s.mulkutkar@somaiya.edu>
2021-10-13 16:02:13 +0530

    added asset images

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,7 +5,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
       
     );
   }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
-  
-  void gotoHome() async {
-    await new Future(() => Duration(seconds: 2));
-    Navigator.pushNamed(context, '/home');
+  const Loading({Key? key}) : super(key: key);
 
   @override
   _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+  void gotoHome() async {
+    await Future(() => const Duration(seconds: 2));
+    Navigator.pushNamed(context, '/home');
+  }
+
   @override
   Widget build(BuildContext context) {
+    
     return const Scaffold(
       backgroundColor: Colors.black,
       body: Center(
