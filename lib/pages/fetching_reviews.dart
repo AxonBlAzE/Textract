960439c38ce522e3a497f48b35536ea5ef7f9142
AxonBlAzE <s.mulkutkar@somaiya.edu>
2021-12-01 12:31:32 +0530

    fixed no internet page for all reviews

--- lib/pages/fetching_reviews.dart
+++ lib/pages/fetching_reviews.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+import 'dart:async';
 // ignore: import_of_legacy_library_into_null_safe
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:textract/pages/error_network.dart';
 
 class Fetching extends StatefulWidget {
   const Fetching({Key? key}) : super(key: key);
@@ -18,6 +21,59 @@ class _FetchingState extends State<Fetching> {
   final String msg = "Fetching Reviews";
   var data;
   bool flag = false;
+  bool internet = false;
+  late StreamSubscription<ConnectivityResult> syncer;
+
+  @override
+  Future<void> connection1() async {
+    bool previous = internet;
+    try {
+      final result = await InternetAddress.lookup("google.com");
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        internet = true;
+      } else {
+        internet = false;
+      }
+    } on Exception catch (_) {
+      setState(() {
+        internet = false;
+      });
+    }
+    if (previous != internet) {
+      setState(() {
+        // internet ? Feed() : null;
+        internet = internet;
+        
+      });
+      // print(internet);
+    }
+  }
+
+    Future<void> connection2() {
+    return connection1();
+  }
+
+  Future<void> connection() async {
+    var Result = await (Connectivity().checkConnectivity());
+    if (Result == ConnectivityResult.mobile) {
+      return connection2();
+    } else if (Result == ConnectivityResult.wifi) {
+      return connection2();
+    } else {
+      setState(() {
+        internet = false;
+      });
+    }
+  }
+
+  Future<void> _connection_change(ConnectivityResult result) async {
+    connection();
+  }
+
+  dispose() {
+    super.dispose();
+    syncer.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,14 +88,18 @@ class _FetchingState extends State<Fetching> {
       });
     }
 
-    void gotoViewFeedback() async {
+    gotoViewFeedback() async {
       // String text = 'none';
       // print('text: $text');
       // if(text.isEmpty){
       //   print('empty');
       // }
+      await connection();
+      if(internet == false){
+        Navigator.popAndPushNamed(context, '/errornetwork');
+      } 
       await getFeedBack();
-      await Future.delayed(const Duration(seconds: 2), () async {
+      await Future.delayed(const Duration(seconds: 1), () async {
         // String text = await getText();
         Navigator.popAndPushNamed(context, '/viewfeed', arguments: {
           'data': data,
